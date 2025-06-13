92aeea338cd7473fc7eb35e4562a71ed8e2d1e37
04amanrajj <04.aman.raj@gmail.com>
2025-06-13 15:34:16 +0530

    update: sort by pkgs by rating

--- backend/controllers/packageController.js
+++ backend/controllers/packageController.js
@@ -3,10 +3,12 @@ const { mockPackages } = require("../constants");
 
 const getTopSellingPackages = async (req, res) => {
   try {
-    
+    let packages = await Package.find().sort({ rating: -1 });
+    if (packages.length === 0) {
       await Package.insertMany(mockPackages);
-      const packages = mockPackages;
-    
+      packages = mockPackages;
+    }
+
     res.json(packages);
   } catch (error) {
     console.error("Package fetch error:", error.message);
